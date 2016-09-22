ffc77122a60de5c8e5746c519b2b8418ebdba8df
chuanyuangit <461346605@qq.com>
2016-09-22 12:33:55 +0800

    修改注册登陆的界面，界面图标变小了

--- app/src/main/java/com/sibo/fastsport/Login.java
+++ app/src/main/java/com/sibo/fastsport/Login.java
@@ -1,16 +1,31 @@
 package com.sibo.fastsport;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Administrator on 2016/9/21 0021.
  */
 public class Login extends AppCompatActivity {
+
+    TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        register = (TextView) findViewById(R.id.tv_register);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Register.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/sibo/fastsport/Register.java
+++ app/src/main/java/com/sibo/fastsport/Register.java
@@ -1,23 +1,27 @@
 package com.sibo.fastsport;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
  * Created by Administrator on 2016/9/21 0021.
  */
 public class Register extends AppCompatActivity{
-    Button register;
+    Button takeIndentify;
+    ImageView back;
     TextView sendIndentify,receiverSecond,remainSecond,resetIndentify;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        register = (Button) findViewById(R.id.btn_register);
+        back = (ImageView) findViewById(R.id.iv_back);
+        takeIndentify = (Button) findViewById(R.id.takeIndentify);
         sendIndentify = (TextView) findViewById(R.id.tv_sendIdentify);
         receiverSecond = (TextView) findViewById(R.id.tv_receiverSecond);
         remainSecond = (TextView) findViewById(R.id.tv_remainSecond);
@@ -26,13 +30,19 @@ public class Register extends AppCompatActivity{
         receiverSecond.setVisibility(View.GONE);
         remainSecond.setVisibility(View.GONE);
         resetIndentify.setVisibility(View.GONE);
-        register.setOnClickListener(new View.OnClickListener() {
+        takeIndentify.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sendIndentify.setVisibility(View.VISIBLE);
                 receiverSecond.setVisibility(View.VISIBLE);
                 remainSecond.setVisibility(View.VISIBLE);
                 resetIndentify.setVisibility(View.VISIBLE);
+            }
+        });
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this,Login.class));
             }
         });
     }
