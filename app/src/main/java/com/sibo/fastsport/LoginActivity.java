e86d7b3fe073975d31a86a7067f0a8e8de0db532
yyuand <yyuan.d@outlook.com>
2016-09-23 13:44:49 +0800

    1、找到title中id

--- app/src/main/java/com/sibo/fastsport/LoginActivity.java
+++ app/src/main/java/com/sibo/fastsport/LoginActivity.java
@@ -3,7 +3,9 @@ package com.sibo.fastsport;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -12,16 +14,35 @@ import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity {
 
     TextView register;
+    android.support.v7.widget.Toolbar loginTitle;
+    private ImageView ivClose;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        register = (TextView) findViewById(R.id.tv_register);
+        initView();
+
+
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+    }
+
+    private void initView() {
+
+        loginTitle = (Toolbar) findViewById(R.id.title);
+        register = (TextView) findViewById(R.id.tv_register);
+        ivClose = (ImageView) loginTitle.findViewById(R.id.iv_close_titlebar);
+
+        ivClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
